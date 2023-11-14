bc8984dec746acf59383f0ff30206feca7f923f4
Clayton Wilson <eclawils@amazon.com>
2023-11-14 06:40:44 -0600

    Remove comments update styles

--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import QueueTabs from "../components/QueueTabs.svelte";
   import QueueList from "../components/QueueList.svelte";
+  import { plannedQueue } from "../stores";
   let selectedTab = "Queue";
 </script>
 
@@ -10,29 +11,12 @@
       on:tabclick={(e) => (selectedTab = e.detail.selected)}
       bind:selected={selectedTab}
     />
-    <QueueList />
+    <QueueList queue={$plannedQueue} />
   </div>
 </div>
 
 <style>
-  /* #container {
-    display: inline-block;
-    background: #ffffff;
-    color: #643045;
-    font-weight: bold;
-    padding: 5px 10px;
-    font-size: 13px;
-    border-radius: 4px;
-  } */
-
-  /* #content {
-    border: 1px solid red;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 400px;
-    width: 300px;
-    flex-direction: column;
-    background-color: wheat;
-  } */
+  .content {
+    height: 320px;
+  }
 </style>
