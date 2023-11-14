d8069074738e0abf679d85f5172fa1611f5acdc1
Clayton Wilson <eclawils@amazon.com>
2023-11-14 06:41:00 -0600

    Display items in queue

--- src/components/QueueList.svelte
+++ src/components/QueueList.svelte
@@ -1,41 +1,60 @@
 <script lang="ts">
   import type AwaitedQueueProcessor from "../lib/AwaitedQueueProcessor";
+  import type { APIAction } from "../types/api";
 
-  export let queue;
+  export let queue: AwaitedQueueProcessor<
+    APIAction,
+    Tampermonkey.Response<object>
+  >;
 </script>
 
-<section />
+<section>
+  {#each queue.items as item, index}
+    <div>
+      <span class="item-status">{item.status}</span>
+      <span class="item-message">{item.displayMessage}</span>
+      <span
+        class="item-delete"
+        on:click={() => {
+          queue.removeAt(index);
+          queue = queue;
+        }}
+        on:keydown={() => {}}>Delete</span
+      >
+      <span> {index}</span>
+    </div>
+  {/each}
+</section>
 
 <style>
   section {
     overflow-y: scroll;
     overflow-x: hidden;
-    height: 340px;
+    height: 100%;
   }
 
   div {
     display: grid;
     width: 100%;
     column-gap: 5px;
-    grid-template-columns: 10% 35% 5% 45%;
+    /* grid-template-columns: 10% 35% 5% 45%; */
+    grid-template-columns: 20% 60% 12% 8%;
     border: 1px solid black;
-    color: black;
     height: 30px;
   }
 
-  .action {
+  .item-status {
     color: blue;
   }
 
-  .profile-name {
-    color: green;
+  .item-message {
+    color: #fff;
   }
 
-  .room-name {
-    color: purple;
-  }
-
-  .failure-reason {
+  .item-delete {
     color: red;
+    cursor: pointer;
+    text-decoration: underline;
+    user-select: none;
   }
 </style>
