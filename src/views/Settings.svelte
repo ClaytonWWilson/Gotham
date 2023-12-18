68993d38c82ea390b3770828c2e1f38bc79bf0db
Clayton Wilson <eclawils@amazon.com>
2023-12-18 02:43:48 -0600

    Switch settings to use SearchableChecklist component

--- src/views/Settings.svelte
+++ src/views/Settings.svelte
@@ -1,77 +1,40 @@
 <script lang="ts">
+  import SearchableChecklist from "../components/SearchableChecklist.svelte";
+  import { defaultFilters } from "../lib/defaults";
   import { STATION_NAME_REGEX } from "../lib/utilites";
   import { roomList, settings } from "../stores";
-  import type { JobChecklistItem } from "../types/ui";
+  import type { FilterRule, JobChecklistItem } from "../types/ui";
 
   let autoHideRoomsListVisible = false;
-  let selectedRoomsCount = 0;
   let roomsChecklist: JobChecklistItem[] = [];
 
   roomList.subscribe((rooms) => {
-    if (!rooms.loading) {
-      roomsChecklist = rooms.rooms.map((room) => {
-        return {
-          name: room.Name,
-          id: room.RoomId,
-          checked: $settings.autoHideRooms.has(room.RoomId),
-        };
-      });
-    }
+    roomsChecklist = rooms.rooms.map((room) => {
+      return {
+        name: room.Name,
+        id: room.RoomId,
+        checked: $settings.autoHideRooms.has(room.RoomId),
+      };
+    });
   });
 
-  function selectAll(list: JobChecklistItem[]) {
-    for (let i = 0; i < list.length; i++) {
-      list[i].checked = true;
-      $settings.autoHideRooms.add(list[i].id);
-    }
-    $settings = $settings;
-  }
-
-  function selectNone(list: JobChecklistItem[]) {
-    for (let i = 0; i < list.length; i++) {
-      list[i].checked = false;
-      $settings.autoHideRooms.clear();
-    }
-    $settings = $settings;
-  }
-
-  function selectRegex(list: JobChecklistItem[], regex: RegExp) {
-    for (let i = 0; i < list.length; i++) {
-      if (list[i].name.match(regex)) {
-        list[i].checked = true;
-        $settings.autoHideRooms.add(list[i].id);
-      }
-    }
-    $settings = $settings;
-  }
-
-  function roomSelectHandler(roomItem: JobChecklistItem) {
-    roomItem.checked = !roomItem.checked;
-
-    if (roomItem.checked) {
-      selectedRoomsCount++;
-      $settings.autoHideRooms.add(roomItem.id);
-    } else {
-      selectedRoomsCount--;
-      $settings.autoHideRooms.delete(roomItem.id);
-    }
+  function roomSelectHandler(roomItems: JobChecklistItem[]) {
+    $settings.autoHideRooms.clear();
+    roomItems.forEach((room) => $settings.autoHideRooms.add(room.id));
     roomsChecklist = roomsChecklist;
 
     $settings = $settings;
   }
 
-  $: filteredRoomCheckList = roomsChecklist.filter((room) => {
-    if (!roomSearch) return true;
-
-    return room.name.toLowerCase().match(roomSearch.toLowerCase());
-  });
-
-  let roomSearch: string | undefined;
-
   function waitTimeChangeHandler(e: Event) {
     const selectEl = e.target as HTMLSelectElement;
     $settings.autoHideWaitSeconds = parseInt(selectEl.value) * 60;
   }
+
+  const filterRules: FilterRule[] = [
+    ...defaultFilters,
+    { name: "Stations", matcher: STATION_NAME_REGEX },
+  ];
 </script>
 
 <div class="container">
@@ -107,60 +70,17 @@
   </div>
   <div class="auto-hide-rooms-container">
     {#if autoHideRoomsListVisible}
-      <h3>Rooms: {roomsChecklist.length}</h3>
-      <div>
-        <span style="font-size: 8pt; color: gray;"
-          >selected: {selectedRoomsCount}</span
-        >
-        <div style="float:right;">
-          <button
-            class="select-button"
-            on:click={() => {
-              selectAll(roomsChecklist);
-              selectedRoomsCount = roomsChecklist.length;
-              roomsChecklist = roomsChecklist;
-            }}>All</button
-          >
-          <button
-            class="select-button"
-            on:click={() => {
-              selectNone(roomsChecklist);
-              selectedRoomsCount = 0;
-              roomsChecklist = roomsChecklist;
-            }}>None</button
-          >
-          <button
-            class="select-button"
-            on:click={() => {
-              selectRegex(roomsChecklist, STATION_NAME_REGEX);
-              selectedRoomsCount = roomsChecklist.filter(
-                (room) => room.checked
-              ).length;
-              roomsChecklist = roomsChecklist;
-            }}>Stations</button
-          >
-        </div>
-      </div>
-      <input type="search" class="search-input" bind:value={roomSearch} />
-      <div class="station-list">
-        {#if $roomList.loading}
-          Loaded {$roomList.rooms.length} rooms...
-        {:else}
-          <ul>
-            {#each filteredRoomCheckList as room}
-              <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-              <li
-                on:click={() => roomSelectHandler(room)}
-                on:keydown={() => {}}
-              >
-                <div class="list-item-wrapper">
-                  <input type="checkbox" checked={room.checked} />
-                  <span style="text-wrap: nowrap;">{room.name}</span>
-                </div>
-              </li>
-            {/each}
-          </ul>
-        {/if}
+      <div class="checklist-container">
+        <SearchableChecklist
+          itemString="Rooms"
+          {filterRules}
+          loading={$roomList.loading}
+          bind:checklistItems={roomsChecklist}
+          on:select={(data) => {
+            const selectedRooms = data.detail.selected;
+            roomSelectHandler(selectedRooms);
+          }}
+        />
       </div>
     {/if}
   </div>
@@ -172,11 +92,6 @@
     background-color: #6b6b6b;
   }
 
-  h3 {
-    margin-top: 0px;
-    margin-bottom: 0px;
-  }
-
   select {
     color: #fff;
     background-color: #6b6b6b;
@@ -186,43 +101,23 @@
     user-select: none;
   }
 
-  ul {
-    cursor: pointer;
-    list-style-type: none;
-    padding-left: 0;
-    margin-top: 0;
-    margin-bottom: 0;
-    max-width: 400px;
-    overflow-x: hidden;
-  }
-
   .auto-hide-rooms-container {
     display: flex;
     flex-direction: column;
     flex-grow: 1;
   }
 
+  .checklist-container {
+    display: grid;
+    grid-template-rows: 200px;
+  }
+
   .container {
     color: white;
   }
 
-  .list-item-wrapper {
-    display: flex;
-    border-bottom: 1px solid gray;
-  }
-
   .request-wait {
     width: 40px;
-    color: white;
-    background-color: #6b6b6b;
-  }
-
-  .search-input {
-    color: white;
-    background-color: #6b6b6b;
-  }
-
-  .select-button {
     color: white;
     background-color: #6b6b6b;
   }
@@ -233,10 +128,5 @@
     column-gap: 5px;
     padding-bottom: 5px;
     padding-top: 5px;
-  }
-
-  .station-list {
-    height: 200px;
-    overflow-y: scroll;
   }
 </style>
