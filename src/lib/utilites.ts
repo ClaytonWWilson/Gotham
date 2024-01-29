6c1eb9d0ddf5016f118bc94649d49be7fbfc81d4
Clayton Wilson <eclawils@amazon.com>
2024-01-29 05:21:31 -0600

    Reorganize functions in utilities to prevent circular dependencies

--- src/lib/utilites.ts
+++ src/lib/utilites.ts
@@ -1,5 +1,3 @@
-import type { APIAction, APIRequest } from "../types/api";
-
 export const STATION_NAME_REGEX =
   /^\s?((AMXL)?.?[A-Z]{3}[1-9]{1}.?.?(-|–).?.Central[\s]?Ops.?(-|–).?[A-Za-z]+)|DON3 - East - Central Ops$/g;
 
@@ -23,83 +21,6 @@ export const INVITE_ERRORS_MAP = {
 };
 
 export const HIDE_ROOMS_ERRORS_MAP = {};
-
-/**
- * @description Wrapper for GM_xmlhttpRequest that makes it promise-based instead of callback-based
- * @param {Tampermonkey.Request.<object>} request The request to be sent
- * @returns {Tampermonkey.Response<object>} The response of GM_xmlhttpRequest whether it resolves or rejects
- */
-async function sendAsyncRequest(
-  request: Tampermonkey.Request
-): Promise<Tampermonkey.Response<object>> {
-  return new Promise((resolve, reject) => {
-    GM_xmlhttpRequest({
-      ...request,
-      onload: (res) => {
-        if (res.status > 100 && res.status < 400) {
-          resolve(res);
-        } else {
-          reject(res);
-        }
-      },
-    });
-  });
-}
-
-export async function sendApiRequest(
-  params: APIRequest
-): Promise<Tampermonkey.Response<object>> {
-  return new Promise(async (resolve, reject) => {
-    console.log("Sending request", params);
-    let apiToken = localStorage.getItem("X-Chime-Auth-Token");
-
-    if (!apiToken) {
-      console.error("API token is missing from browser storage.");
-      reject("API token is missing from browser storage.");
-    }
-
-    apiToken = apiToken.replace(/"/g, "");
-
-    apiToken = `_aws_wt_session=${apiToken}`;
-
-    let requestParams: Tampermonkey.Request<object> = {
-      url: params.endpoint,
-      method: params.method,
-      headers: {
-        "x-chime-auth-token": apiToken,
-      },
-    };
-
-    if (params.payload) {
-      requestParams.data = JSON.stringify(params.payload);
-      requestParams.headers = {
-        ...requestParams.headers,
-        "Content-Type": "application/json",
-      };
-    }
-
-    try {
-      const response = await sendAsyncRequest(requestParams);
-      return resolve(response);
-    } catch (error) {
-      reject(error);
-    }
-  });
-}
-
-export async function processAPIAction(
-  action: APIAction
-): Promise<Tampermonkey.Response<object>> {
-  action.startedAt = new Date();
-  action.status = "RUNNING";
-
-  const response = await sendApiRequest(action.request);
-
-  action.status = "COMPLETE";
-  action.finishedAt = new Date();
-
-  return response;
-}
 
 export function truncateString(
   s: string,
